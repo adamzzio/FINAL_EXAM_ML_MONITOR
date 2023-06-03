639fac6e8e511e77849934b4d33e551a03f8db61
adamzzio <120027998+adamzzio@users.noreply.github.com>
2023-06-03 20:11:04 +0700

    Create update_model.py

---
+++ update_model.py
@@ -0,0 +1,26 @@
+import pickle
+from pathlib import Path
+from github import Github
+import os
+from dotenv import load_dotenv
+
+def update(updated_model):
+  # Replace 'YOUR_ACCESS_TOKEN' with your actual access token
+  access_token = os.environ.get('PAT')
+  # Create a PyGithub instance with your access token
+  g = Github(access_token)
+
+  repo_owner = 'adamzzio'
+  repo_name = 'FINAL_EXAM_ML'
+  file_path = 'model/decision_tree_model_test.sav'
+
+  # Get the repository object
+  repo = g.get_user(repo_owner).get_repo(repo_name)
+
+  # Get the contents of the model file as bytes
+  file_content = repo.get_contents(file_path)
+  existing_sha = file_content.sha
+  file_content = file_content.decoded_content
+  
+  repo.update_file(file_path, "Updated model file", updated_model, existing_sha)
+
