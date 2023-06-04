8ecae01a2e31a659bd77f02fb2a6324567ae79b9
adamzzio <120027998+adamzzio@users.noreply.github.com>
2023-06-04 11:42:22 +0700

    Update update_model.py

--- update_model.py
+++ update_model.py
@@ -5,6 +5,24 @@ from github import Github
 import os
 from dotenv import load_dotenv
 import streamlit as st
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Retrain the model with new data
+new_model = DecisionTreeClassifier(random_state=42)
+new_model.fit(X, y)
+
+# Update the existing model object with the new model
+model = new_model
+
+
+# Convert the model to binary content
+updated_model_content = pickle.dumps(model)
+
 
 def update(updated_model):
   # Replace 'YOUR_ACCESS_TOKEN' with your actual access token
@@ -23,11 +41,12 @@ def update(updated_model):
   file_content = repo.get_contents(file_path)
   existing_sha = file_content.sha
   # file_content = file_content.decoded_content
-  st.write(file_content)
-  st.write(file_path)
-  st.write(existing_sha)
-  st.write(access_token)
-  repo.update_file(file_content.path, "Updated model file", updated_model, existing_sha, branch="main")
-  st.write(github.enable_console_debug_logging())
+#   st.write(file_content)
+#   st.write(file_path)
+#   st.write(existing_sha)
+#   st.write(access_token)
+  repo.update_file(file_path, "Updated model file", updated_model, existing_sha, branch="main")
+#   st.write(github.enable_console_debug_logging())
   # repo.create_file("test.txt", "test", "test")
 
+update(updated_model_content)
